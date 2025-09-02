6f6a845c794f9025022e9570d27b76f0d3c62ec1
power2loop <kumar.vkvikash3@gmail.com>
2025-09-03 01:10:21 +0530

    render update 3

--- Frontend/src/services/socket/socket.js
+++ Frontend/src/services/socket/socket.js
@@ -1,6 +1,7 @@
 // src/services/realtime/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // connect to backend
+const socket = io("https://whiteboard-1kfc.onrender.com/"); // connect to backend
+
 
 export default socket;
